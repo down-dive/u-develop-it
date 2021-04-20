356253bda56b40fcf92fb8a6675601790cd47f07
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-04-20 15:39:15 -0700

    adding db calls

--- server.js
+++ server.js
@@ -4,29 +4,34 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Connect database
+// Connect to database
 const db = mysql.createConnection(
     {
-        host: 'localhost',
-        // Your MySQL username,
-        user: 'root',
-        // Your password
-        password: 'password',
-        database: 'election'
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
     },
-    console.log('Connectd to the election database')
-);
+    console.log('Connected to the election database.')
+  );
 
-  // Default respose for any other request (Not Found)
-  app.use((req, res) => {
-      res.status(404).end();
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
   });
   
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
