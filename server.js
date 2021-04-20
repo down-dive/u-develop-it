3f71cf3f08970de43ec963c04afad975ea3a5bec
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-04-20 14:53:41 -0700

    connecting to mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -5,6 +6,19 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//Connect database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your password
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connectd to the election database')
+);
 
   // Default respose for any other request (Not Found)
   app.use((req, res) => {
